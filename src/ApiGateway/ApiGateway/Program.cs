ac33ea13c37e2180bf1f18f207ac3a95bd29e20e
InnaHunchenko <InnaHunchenko@gmail.com>
2025-03-12 13:48:00 +0200

    fix

--- src/ApiGateway/ApiGateway/Program.cs
+++ src/ApiGateway/ApiGateway/Program.cs
@@ -7,7 +7,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-           policy.WithOrigins("https://coffeeshop-front.mangodune-bc89c06f.polandcentral.azurecontainerapps.io")
+           policy.WithOrigins("https://coffeesshop.org/")
             //policy.WithOrigins("http://localhost:4200")
                   .AllowAnyMethod()
                   .AllowAnyHeader()
